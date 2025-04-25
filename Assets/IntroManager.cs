03f0b9a236a897f661bd86f87f9b94c9e3fb2d35
mysticmanikk <kmanikgill@gmail.com>
2025-04-25 00:54:24 -0400

    Updated video player to stream from URL for WebGL compatibility

--- Assets/IntroManager.cs
+++ Assets/IntroManager.cs
@@ -6,18 +6,35 @@ public class IntroManager : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
 
+    // Google Drive shareable link converted to direct URL
+    public string videoURL = "https://drive.google.com/uc?export=download&id=1K2p3srYPJFows_q4XxMD1lHbnGeWXWga";
+
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
 
-        // Register a callback for when the video finishes
+        // Set the source to URL
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = videoURL;
+
+        // Register callback when the video finishes
         videoPlayer.loopPointReached += OnVideoEnd;
+
+        // Prepare the video before playing
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += OnVideoPrepared;
+    }
+
+    // Called when the video is prepared (ready to play)
+    void OnVideoPrepared(VideoPlayer vp)
+    {
+        vp.Play();
     }
 
     // Called when the video ends
     void OnVideoEnd(VideoPlayer vp)
     {
-        // Load the MainMenu scene
+        // Load the MainMenu scene after the video ends
         SceneManager.LoadScene("MainMenu");
     }
 
@@ -25,5 +42,6 @@ public class IntroManager : MonoBehaviour
     {
         // Unsubscribe to prevent memory leaks
         videoPlayer.loopPointReached -= OnVideoEnd;
+        videoPlayer.prepareCompleted -= OnVideoPrepared;
     }
 }
